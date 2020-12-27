bfa584ba4bd78de98a02d06865f7aa9c1c857fc0
Sippora04 <sippora.toppo95@gmail.com>
2020-12-28 03:16:58 +0530

    Added: creating RESTController to demonstrate the various HTTP methods

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookApplication.java
@@ -5,11 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AddressBookApplication {
-
 	public static void main(String[] args) {
-	
 		SpringApplication.run(AddressBookApplication.class, args);
-		System.out.println("Welcome to Addressbook Spring Project");
 	}
-
 }
