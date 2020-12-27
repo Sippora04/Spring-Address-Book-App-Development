92919380371178709fe68c391850161eba55c9a6
Sippora04 <sippora.toppo95@gmail.com>
2020-12-28 03:08:39 +0530

    Added: creating addressbook to cater to REST request from AddressBook UI

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AddressBookAppApplication {
+public class AddressBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookAppApplication.class, args);
+	
+		SpringApplication.run(AddressBookApplication.class, args);
+		System.out.println("Welcome to Addressbook Spring Project");
 	}
 
 }
