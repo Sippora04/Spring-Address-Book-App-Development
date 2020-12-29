71597953e445abcbb3c8be981540587bf0612444
Sippora04 <sippora.toppo95@gmail.com>
2020-12-30 01:42:09 +0530

    Added: @Slf4j at Class level and Logging levels, files and patterns are handled in application.properties

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookApplication.java
@@ -3,9 +3,13 @@ package com.bridgelabz.addressbookapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class AddressBookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookApplication.class, args);
+		log.info("***** Address Book App Started *****");
 	}
 }
