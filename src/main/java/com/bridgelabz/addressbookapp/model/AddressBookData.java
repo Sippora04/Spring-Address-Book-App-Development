fab637b4eedf089908753f255af8310c6dc5cf08
Sippora04 <sippora.toppo95@gmail.com>
2020-12-30 04:22:21 +0530

    Added: saving data to MySQL Database

--- src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
+++ src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
@@ -1,13 +1,30 @@
 package com.bridgelabz.addressbookapp.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.bridgelabz.addressbookapp.dto.AddressBookDTO;
 
 import lombok.Data;
 
+@Entity
+@Table(name = "address_book")
 public @Data class AddressBookData {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "id")
 	private int id;
+	
+	@Column(name = "name")
 	private String name;
 	private String address;
+	
+	@Column(name = "phone_number")
 	private String phoneNumber;
 	private String city;
 	private String state;
--- src/main/java/com/bridgelabz/addressbookapp/service/AddressBookService.java
+++ src/main/java/com/bridgelabz/addressbookapp/service/AddressBookService.java
@@ -3,17 +3,25 @@ package com.bridgelabz.addressbookapp.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bridgelabz.addressbookapp.dto.AddressBookDTO;
 import com.bridgelabz.addressbookapp.exceptions.AddressBookException;
 import com.bridgelabz.addressbookapp.model.AddressBookData;
+import com.bridgelabz.addressbookapp.repository.AddressBookRepository;
+
+import lombok.extern.slf4j.Slf4j;
 
 /*@Service annotation is a specialization of @Component annotation. 
  *Its used with classes that provide some business functionalities. 
  */
 @Service
+@Slf4j
 public class AddressBookService implements IAddressBookService {
+
+	@Autowired
+	private AddressBookRepository addressBookRepository;
 
 	private List<AddressBookData> addressBookDataList = new ArrayList<>();
 
@@ -35,8 +43,9 @@ public class AddressBookService implements IAddressBookService {
 		// TODO Auto-generated method stub
 		AddressBookData addressBookData = null;
 		addressBookData = new AddressBookData((addressBookDataList.size() + 1), addressBookDTO);
+		log.debug("Address Book Data: " + addressBookData.toString());
 		addressBookDataList.add(addressBookData);
-		return addressBookData;
+		return addressBookRepository.save(addressBookData);
 	}
 
 	@Override
