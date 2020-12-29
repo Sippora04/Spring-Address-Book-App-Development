008186f2913e86116e44e3f0a9066719aeb67537
Sippora04 <sippora.toppo95@gmail.com>
2020-12-30 02:48:18 +0530

    Added: validation on Name Field

--- src/main/java/com/bridgelabz/addressbookapp/controllers/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controllers/AddressBookController.java
@@ -2,6 +2,8 @@ package com.bridgelabz.addressbookapp.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -46,7 +48,7 @@ public class AddressBookController {
 	}
 
 	@PostMapping("/create")
-	public ResponseEntity<ResponseDTO> createAddressBookData(@RequestBody AddressBookDTO addressBookDTO) {
+	public ResponseEntity<ResponseDTO> createAddressBookData(@Valid @RequestBody AddressBookDTO addressBookDTO) {
 		AddressBookData addressbookData = null;
 		addressbookData = addressBookService.createAddressBookData(addressBookDTO);
 		ResponseDTO responseDTO = new ResponseDTO("Created AddressBook Data Successfully", addressbookData);
